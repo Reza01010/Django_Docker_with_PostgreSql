2b2fd9d546de1afeb722148c47ccafe110d407e4
Reza01010 <hi95534reza@gmail.com>
2023-03-05 17:42:14 +0330

    add pages view and test to app pages

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+
+class AboutUsPagesView(TemplateView):
+    template_name = 'pages/aboutus.html'
+
+
