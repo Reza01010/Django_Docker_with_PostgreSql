8160d983b540c9b9203a976fa13a168c0dbc3101
Reza01010 <hi95534reza@gmail.com>
2023-03-08 17:04:02 +0330

    Add email confirm.html and email_confirmation_subject.txt and email_confirmation message.txt to the project

--- config/settings.py
+++ config/settings.py
@@ -64,6 +64,14 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+#                              If you're up, you've used email.
+# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+# EMAIL_HOST = "smtp.gmail.com"
+# EMAIL_USE_TLS = True
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = "your email address"
+# EMAIL_HOST_PASSWORD = "your email password"
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
